c7032d8b227de317fe323136cfd985a956d55045
clk1984 <fernandoarozarena84@gmail.com>
2024-05-29 18:33:49 +0200

    [Gordots][Menus] WIP settings menu

--- src/Scripts/PauseMenu.gd
+++ src/Scripts/PauseMenu.gd
@@ -1,13 +1,18 @@
 extends Control
 
+@onready var pause_menu : GridContainer = get_node("PauseGridContainer")
+@onready var settings_menu : GridContainer = get_node("SettingsGridContainer")
+
 func _ready():
 	process_mode= Node.PROCESS_MODE_ALWAYS 
-
+	
 func _on_resume_button_pressed():
-	pass # Replace with function body.
+	GameStateMachine.current_state.Transitioned.emit(GameStateMachine.current_state, 'startlevelstate')
+	queue_free()
 
 func _on_settings_button_pressed():
-	pass # Replace with function body.
+	pause_menu.hide()
+	settings_menu.show() # Replace with function body.
 
 func _on_quit_button_pressed():
 	get_tree().quit()
--- src/state_machine/GameStateMachine/states/PauseState.gd
+++ src/state_machine/GameStateMachine/states/PauseState.gd
@@ -3,6 +3,7 @@ var PauseMenu
 var root_node: Node2D
 var paused
 
+
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
@@ -11,10 +12,12 @@ func enter():
 	PauseMenu = root_node.get_tree().get_first_node_in_group('current_scene').get_node("PauseMenu")
 	paused = true
 	PauseMenu.show()
+	PauseMenu.get_node("PauseGridContainer").show()
 	root_node.get_tree().get_first_node_in_group('current_scene').get_tree().paused = paused
 
 func exit():
 	paused = false
+	PauseMenu.get_node("SettingsGridContainer").hide()
 	PauseMenu.hide()
 	root_node.get_tree().get_first_node_in_group('current_scene').get_tree().paused = paused
 
